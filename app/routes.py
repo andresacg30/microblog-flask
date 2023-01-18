7cc878d9079752b80fa67468e700bffdc1beab83
Andres Cabrera <ext.acabrera@warbyparker.com>
2023-01-18 08:51:26 -0400

    add bootstrap styling to templates

--- app/routes.py
+++ app/routes.py
@@ -7,7 +7,7 @@ from app import app, db
 from app.forms import LoginForm, RegistrationForm, EditProfileForm, EmptyForm, PostForm
 from app.forms import ResetPasswordRequestForm, ResetPasswordForm
 from app.models import User, Post
-from app.email import send_password_reset_email
+from app.emails import send_password_reset_email
 
 
 @app.route('/', methods=['GET', 'POST'])
